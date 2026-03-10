b354ed5eb229be4fc1bbb1108f994ec474813a09
likkkyu <likekeyuna@outlook.com>
2026-03-11 02:08:29 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,10 +2,9 @@ import sys
 import os
 from mangum import Mangum
 
-# 将 backend 所在的根目录加入 Python 路径
+# 这里的关键：让 Python 能够跨文件夹找到你的 main.py
+# 路径必须对应 GitHub 上的 backend/backend/main.py
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
-# 这里的导入路径必须和你 GitHub 上的 backend/backend/main.py 一致
 from backend.backend.main import app
 
 handler = Mangum(app)
